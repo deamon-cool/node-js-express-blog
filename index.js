c35e06c80d7e262d90de770c7d05cc75260e25ce
daemon-cool <damian.damian.tofik@gmail.com>
2020-02-22 14:43:58 +0100

    Created a home page according to clean blog bootstrap

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/index.html'));
+});
 
 app.listen(3000, () => {
     console.log(`Server running at http://${'127.0.0.1'}:${3000}/`);
