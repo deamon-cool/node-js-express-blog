275f9dc636905ad4ee6263ed923bd4eb5a505c81
daemon-cool <damian.damian.tofik@gmail.com>
2020-03-05 18:04:08 +0100

    Created middleware function that checking validation of inputs created post (if there are any empty). Thist function call before app.post

--- index.js
+++ index.js
@@ -23,12 +23,14 @@ app.set('views', `${__dirname}/views`);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-const customMiddleware = (req, res, next) => {
-    console.log('I HAVE BEEEN CAALLLED :) ');
+
+const validateCreatePostMiddleware = (req, res, next) => {
+    if(!req.files || !req.body.username || !req.body.title || !req.body.subtitle || !req.body.content) {
+        return res.redirect('/posts/new');
+    }
+
     next();
 };
-
-app.use(customMiddleware);
 app.use('/posts/store', validateCreatePostMiddleware);
 
 
