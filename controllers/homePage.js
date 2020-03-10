f621ddd6169c20d561d95cb56d19860b2ab2e39f
daemon-cool <damian.damian.tofik@gmail.com>
2020-03-10 14:53:14 +0100

    check through log if session works correctly

--- controllers/homePage.js
+++ controllers/homePage.js
@@ -3,5 +3,7 @@ const Post = require('../database/models/Post');
 module.exports = async (req, res) => {
     let posts = await Post.find();
 
+    console.log(req.session);
+
     res.render('index', {posts});
 };
