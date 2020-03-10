9f3124460a84d2cdd647873fa2a494bb739e9306
daemon-cool <damian.damian.tofik@gmail.com>
2020-03-10 16:26:18 +0100

    This is configuration for holding session for user when server stop and run again. It means user doesn't have to log again

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const fileUpload = require('express-fileupload');
 const expressSession = require('express-session');
+const connectMongo = require('connect-mongo');
 
 const createPostController = require('./controllers/createPost');
 const homePageController = require('./controllers/homePage');
@@ -17,8 +18,13 @@ const loginUserController = require('./controllers/loginUser');
 
 const app = new express();
 
+const mongoStore = connectMongo(expressSession);
+
 app.use(expressSession({
-    secret: 'secret'
+    secret: 'secret',
+    store: new mongoStore({
+        mongooseConnection: mongoose.connection
+    })
 }));
 
 
