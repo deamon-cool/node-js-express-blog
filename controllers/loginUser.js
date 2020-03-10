409d5dd78d20616dc68cf9dce1b6218a422cfd9e
daemon-cool <damian.damian.tofik@gmail.com>
2020-03-10 13:59:55 +0100

    Added in loginUser.js checking validation of user. If user properly log in then will be bring to homepage, if not that means email or password are incorrect and they are not in database. IMPORTANT: function compare() comparing encrypted passwords. So don't unencrypted any passwords frm database

--- controllers/loginUser.js
+++ controllers/loginUser.js
@@ -1,5 +1,23 @@
+const bcrypt = require('bcrypt');
 const User = require('../database/models/User');
 
 module.exports = (req, res) => {
-    res.redirect('/');
+    const {email, password} = req.body;
+
+    // find user through email
+    User.findOne({email}, (error, user) => {
+        if(user) {
+            // Compare passwords - there are the same ? User is true
+            bcrypt.compare(password, user.password, (error, result) => {
+                if(result) {
+                    // store user session
+                    res.redirect('/');
+                } else {
+                    res.redirect('/auth/login');
+                }
+            });
+        } else {
+           return res.redirect('/auth/login');
+        }
+    });
 };
