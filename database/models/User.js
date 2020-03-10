046f6d77bd93ab0b12b3a993c8ec2409403daa3e
daemon-cool <damian.damian.tofik@gmail.com>
2020-03-10 12:28:10 +0100

    Gave validation in database thanks mongoose. Now all inputs in page are valid and if any will be empty then document won't be save to database

--- database/models/User.js
+++ database/models/User.js
@@ -2,10 +2,21 @@ const bcrypt = require('bcrypt');
 const mongoose = require('mongoose');
 
 mongoose.set('useCreateIndex', true);
+
 const UserSchema = new mongoose.Schema({
-    username: String,
-    email: String,
-    password: String
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
 });
 
 //middleware
