6587d32fb13bbf2f2021e64b605447bc50703688
daemon-cool <damian.damian.tofik@gmail.com>
2020-03-12 17:26:31 +0100

    Created customized messages for empty inputs register errors

--- database/models/User.js
+++ database/models/User.js
@@ -6,16 +6,16 @@ mongoose.set('useCreateIndex', true);
 const UserSchema = new mongoose.Schema({
     username: {
         type: String,
-        required: true
+        required: [true, 'Please provide your username']
     },
     email: {
         type: String,
-        required: true,
+        required: [true, 'Please provide your email'],
         unique: true
     },
     password: {
         type: String,
-        required: true
+        required: [true, 'Please provide your password']
     }
 });
 
