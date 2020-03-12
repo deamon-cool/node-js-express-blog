ed7d9fa1f6ca93a7e66da99864d89cca2958b241
daemon-cool <damian.damian.tofik@gmail.com>
2020-03-12 16:07:55 +0100

    Added valid authentication of user if he wants to create new post. If ins't  valid then he is redirected to home page

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,4 +1,12 @@
-module.exports = (req, res, next) => {
+const User = require('../database/models/User');
 
+module.exports = (req, res, next) => {
+    User.findById(req.session.userId, (error, user) => {
+        if(error || !user) {
+            return res.redirect('/');
+        }
+
+        next();
+    });
 
 };
